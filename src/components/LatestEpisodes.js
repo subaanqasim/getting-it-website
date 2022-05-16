a495a934c42d0cb3e2521f954c8306fc29776519
Subaan Qasim <96848399+subaanqasim@users.noreply.github.com>
2022-05-16 12:14:25 +0100

    Added buttons and subtitle ot LatestEpisodes

--- src/components/LatestEpisodes.js
+++ src/components/LatestEpisodes.js
@@ -1,14 +1,62 @@
 import React from "react"
-import { Title, Container } from "@mantine/core"
+import {
+  Title,
+  Container,
+  Button,
+  Group,
+  Divider,
+  Box,
+  createStyles,
+} from "@mantine/core"
+import { Link } from "gatsby"
 import EpisodeCard from "./EpisodeCard"
+import { Microphone } from "tabler-icons-react"
+
+const useStyles = createStyles((theme) => ({
+  title: {
+    fontSize: theme.other.fontSizesLarge[1],
+
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      fontSize: theme.other.fontSizesSmall[1],
+    },
+  },
+}))
 
 export default function LatestEpisodes() {
+  const { classes } = useStyles()
+
   return (
-    <Container size="lg">
-      <Title order={2}>Latest Episodes</Title>
+    <Container size="lg" mt="8em">
+      <Divider
+        my="xs"
+        labelPosition="left"
+        label={
+          <>
+            <Microphone size={12} />
+            <Box ml={5} style={{ fontSize: "1rem" }}>
+              Listen to our most recent ramblings
+            </Box>
+          </>
+        }
+      />
+      <Title order={2} className={classes.title}>
+        Latest Episodes
+      </Title>
       <EpisodeCard />
-      <EpisodeCard />
-      <EpisodeCard />
+      <Group position="center" spacing="md" mt="lg">
+        <Button
+          component={Link}
+          to="/episodes"
+          variant="light"
+          size="lg"
+          // color="giBlue"
+        >
+          All episodes
+        </Button>
+        <Button variant="default" size="lg">
+          Load more
+        </Button>
+      </Group>
     </Container>
   )
 }
