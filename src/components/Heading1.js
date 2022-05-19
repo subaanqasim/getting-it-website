d48f59e4be7c0ca696f3d2665219c3a8641208e0
Subaan Qasim <96848399+subaanqasim@users.noreply.github.com>
2022-05-19 10:13:45 +0100

    Create Heading1.js

---
+++ src/components/Heading1.js
@@ -0,0 +1,41 @@
+import { Box, createStyles, Divider, Title } from "@mantine/core"
+import React from "react"
+
+const useStyles = createStyles((theme) => ({
+  title: {
+    fontSize: theme.other.fontSizesLarge[0],
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: theme.other.fontSizesSmall[0],
+    },
+  },
+}))
+
+export default function Heading1({
+  title,
+  subtitle,
+  subtitleIcon,
+  subtitlePosition,
+}) {
+  const { classes } = useStyles()
+
+  return (
+    <>
+      <Divider
+        my="xs"
+        labelPosition={subtitlePosition}
+        label={
+          <>
+            {subtitleIcon}
+            <Box ml={5} style={{ fontSize: "1rem" }}>
+              {subtitle}
+            </Box>
+          </>
+        }
+      />
+      <Title order={1} className={classes.title}>
+        {title}
+      </Title>
+    </>
+  )
+}
