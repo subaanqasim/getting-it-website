7ae023a67870e34d4b33fd6c42ddfd4972f79c04
Subaan Qasim <96848399+subaanqasim@users.noreply.github.com>
2022-06-06 12:00:25 +0100

    Added Formik for contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -9,29 +9,88 @@ import {
   SimpleGrid,
 } from "@mantine/core"
 import useStyles from "./ContactForm.styles"
+import { useFormik } from "formik"
+import * as Yup from "yup"
 
 export default function ContactForm() {
   const { classes, theme } = useStyles()
 
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().max(100, "Must be 100 characters or less"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      subject: Yup.string().required("Please give your message a subject"),
+      message: Yup.string().required(
+        "Oops, looks like you forgot your message"
+      ),
+    }),
+    onSubmit: (values) => {
+      console.log(values)
+    },
+  })
+
   return (
     <Paper shadow="md" radius="sm" className={classes.wrapper}>
-      <form
-        className={classes.form}
-        onSubmit={(event) => event.preventDefault()}
-      >
+      <form className={classes.form} onSubmit={formik.handleSubmit}>
         <Text size="lg" weight={700} className={classes.title}>
           Get in touch via email
         </Text>
 
         <div className={classes.fields}>
           <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
-            <TextInput label="Your name" placeholder="Your name" />
-            <TextInput label="Your email" placeholder="john@doe.com" required />
+            <TextInput
+              {...formik.getFieldProps("name")}
+              error={
+                formik.errors.name && formik.touched.name
+                  ? formik.errors.name
+                  : ""
+              }
+              id="name"
+              label="Your name"
+              placeholder="Your name"
+            />
+            <TextInput
+              {...formik.getFieldProps("email")}
+              error={
+                formik.errors.email && formik.touched.email
+                  ? formik.errors.email
+                  : ""
+              }
+              id="email"
+              label="Your email"
+              placeholder="john@doe.com"
+              required
+            />
           </SimpleGrid>
 
-          <TextInput mt="md" label="Subject" placeholder="Subject" required />
+          <TextInput
+            {...formik.getFieldProps("subject")}
+            error={
+              formik.errors.subject && formik.touched.subject
+                ? formik.errors.subject
+                : ""
+            }
+            id="subject"
+            mt="md"
+            label="Subject"
+            placeholder="Subject"
+            required
+          />
 
           <Textarea
+            {...formik.getFieldProps("message")}
+            error={
+              formik.errors.message && formik.touched.message
+                ? formik.errors.message
+                : ""
+            }
+            id="message"
             mt="md"
             label="Your message"
             placeholder="Getting It is deffo the best podcast out there."
