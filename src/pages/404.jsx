ca86e657eb7e6d5ccb51dd28e98f9bf066ea1781
Subaan Qasim <96848399+subaanqasim@users.noreply.github.com>
2022-06-09 16:07:23 +0100

    Removed 404 styles file from /pages
    During build process a route for 404.styles.js would be created, so put styles into 404 page component to remove styles file

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,56 @@
 import React from "react"
-import { Title, Text, Button, Container, Group, Anchor } from "@mantine/core"
+import {
+  Title,
+  Text,
+  Button,
+  Container,
+  Group,
+  Anchor,
+  createStyles,
+} from "@mantine/core"
 import { Link } from "gatsby"
-import useStyles from "./404.styles"
-import WebpageSeo from "../../components/seo/WebpageSeo"
+import WebpageSeo from "../components/seo/WebpageSeo"
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    paddingTop: 80,
+    paddingBottom: 80,
+  },
+
+  label: {
+    textAlign: "center",
+    fontWeight: 900,
+    fontSize: 220,
+    lineHeight: 1,
+    marginBottom: theme.spacing.xl * 1.5,
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[4]
+        : theme.colors.gray[2],
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: 120,
+    },
+  },
+
+  title: {
+    fontFamily: theme.fontFamily,
+    textAlign: "center",
+    fontWeight: 900,
+    fontSize: 38,
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: 32,
+    },
+  },
+
+  description: {
+    maxWidth: 500,
+    margin: "auto",
+    marginTop: theme.spacing.xl,
+    marginBottom: theme.spacing.xl * 1.5,
+  },
+}))
 
 export default function PageNotFound() {
   const { classes } = useStyles()
